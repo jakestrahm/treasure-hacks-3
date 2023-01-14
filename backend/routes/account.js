72bf6af1c07d95ab7e272ac74c0b70ac18d42230
unknown <kyoshisew@gmail.com>
2023-01-14 14:12:30 -0500

    implemented routes on server.js (see errors)

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { getAccountID, getAccounts } = require("../views/userView");
 
-router.get("/account", getAccounts);
-router.get("/account/:id", getAccountID);
+router.get("/api/account", getAccounts);
+router.get("/api/account/:id", getAccountID);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { newLogger } = require("./utils");
 const app = express();
 const logger = newLogger();
+const accountRoutes = require("./routes/account");
+app.use("/api/account", accountRoutes);
 
 app.listen(3001, () => {
   logger.info("running on port 3001");
@@ -18,17 +20,17 @@ app.post("/api/auth/revoke", (req, res) => {
   res.send({ response: "revoked!" });
 });
 
-app.get("/api/account/", (req, res) => {
-  res.send({ id: 999 });
-});
+// app.get("/api/account/", (req, res) => {
+//   res.send({ id: 999 });
+// });
 
-app.patch("/api/account", (req, res) => {
-  res.send({ account: "updated info!" });
-});
+// app.patch("/api/account", (req, res) => {
+//   res.send({ account: "updated info!" });
+// });
 
-app.post("/api/account", (req, res) => {
-  res.send({ response: "account was made!" });
-});
+// app.post("/api/account", (req, res) => {
+//   res.send({ response: "account was made!" });
+// });
 
 app.get("/api/category/:id", (req, res) => {
   res.send({ id: 999 });
