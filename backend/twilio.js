622965bb10ba96fe5bdb33688149034093d53f8d
unknown <kyoshisew@gmail.com>
2023-01-14 10:01:25 -0500

    added TWILIO ENV variables

--- backend/twilio.js
+++ backend/twilio.js
@@ -1,11 +1,11 @@
-const accountSid = "ACcd21c5cced8333dbaa7fc8a020c115e8";
-const authToken = "8bd8464e7f53918293e5a5224f1d8fad";
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 
 client.messages
   .create({
     body: "Hello from Twilio",
-    from: "+13204411243",
+    from: process.env.TWILIO_PHONE_NUMBER,
     to: "+15167256993",
   })
   .then((message) => console.log(message.sid));
