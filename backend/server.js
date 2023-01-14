693dd108fae7dfc7f5f1ae7572f018db9a9ce37a
unknown <kyoshisew@gmail.com>
2023-01-13 23:04:48 -0500

    added categories endpoints

--- backend/server.js
+++ backend/server.js
@@ -8,16 +8,46 @@ app.get('/',(req,res)=>{
   res.send('root file');
 });
 
-app.get('/api/account',(req,res) =>{
-  res.send({'id' : 999});
+app.get('/api/account/:id',(req,res) =>{
+  res.send({id : 999});
 });
 
+app.get('/api/accounts', (req,res) =>{
+  res.send({
+      accounts : [
+        {id : 123213},
+        {id : 21313}
+      ]
+    }
+  );
+})
+
 app.patch('/api/account',(req,res) =>{
-  res.send({'account' : 'updated info!'})
+  res.send({account : 'updated info!'});
 });
 
 app.post('/api/account',(req,res) =>{
-  res.send({'response' : 'account was made!'})
+  res.send({response: 'account was made!'});
 });
 
+app.get('/api/categories/:id',(req,res) =>{
+  res.send({id: 999});
+});
 
+app.get('/api/categories', (req,res) =>{
+  res.send({
+      categories : [
+        {name : 'school'},
+        {name : 'gym'}
+      ]
+    }
+  );
+});
+
+app.patch('/api/categories/:id',(req,res) =>{
+  res.send({response : 'category was updated'});
+});
+
+app.delete('api/categories/:id',(req,res) =>{
+  res.send({response : 'deleted id'});
+})
