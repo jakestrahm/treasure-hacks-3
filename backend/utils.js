5e50cef1ad71bbd0444e8384fe0fd9f555ca9794
unknown <kyoshisew@gmail.com>
2023-01-14 02:06:41 -0500

    added utils.js with winston

--- backend/utils.js
+++ backend/utils.js
@@ -1,8 +1,8 @@
 const winston = require("winston");
-
-export const newLogger = () => {
-  const logger = winston.createLogger({
+const newLogger = () => {
+  return winston.createLogger({
     level: "info",
     format: winston.format.json(),
   });
 };
+module.exports = { newLogger };
