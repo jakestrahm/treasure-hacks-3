bc3c54ce36c40fce7a076fc030500fedf09ea6a1
unknown <kyoshisew@gmail.com>
2023-01-14 10:17:23 -0500

    twilio api set up with ENV variables

--- backend/twilio.js
+++ backend/twilio.js
@@ -1,11 +1,15 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
-
+const phoneNumber = process.env.TWILIO_PHONE_NUMBER;
+console.log(accountSid, authToken, phoneNumber);
 client.messages
   .create({
-    body: "Hello from Twilio",
-    from: process.env.TWILIO_PHONE_NUMBER,
+    body: "hello with ENV variables!",
+    from: phoneNumber,
     to: "+15167256993",
   })
-  .then((message) => console.log(message.sid));
+  .then((message) => console.log(message.sid))
+  .catch((err) => console.log(err));
