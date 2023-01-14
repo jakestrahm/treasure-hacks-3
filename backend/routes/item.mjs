f63e0aed960fc501f11f2326ba15c3ee6f51558a
unknown <kyoshisew@gmail.com>
2023-01-14 15:59:19 -0500

    added item routes

--- backend/routes/item.mjs
+++ backend/routes/item.mjs
@@ -1,12 +1,8 @@
 import express from "express";
 const router = express.Router();
-
-import {
-  getAllAccounts,
-  getAccountByID,
-} from "../controllers/userController.mjs";
-
-router.get("/", getAllAccounts);
-router.get("/api/account/:id", getAccountByID);
-
+router.get("/:id");
+router.get("/");
+router.post("/:id");
+router.patch("/:id");
+router.delete("/:id");
 export default router;
