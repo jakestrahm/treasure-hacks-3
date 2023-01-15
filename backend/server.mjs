4999bda399b9ca9682f95806d92d806c39c0d1a3
JObog <jobogbaimhe@gmail.com>
2023-01-14 19:25:16 -0500

    created server class

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,8 +1,25 @@
-import express, { Router } from "express";
+import express from "express";
 import { newLogger } from "./utils.mjs";
-const app = express();
-const logger = newLogger();
 
-app.listen(3001, () => {
-  logger.info("running on port 3001");
-});
+class Server {
+  constructor(authRouter, accountRouter, itemRouter, categoryRouter) {
+    //route config
+    this.app = express()
+    this.app.use("/api/auth", authRouter);
+    this.app.use("/api/account", accountRouter);
+    this.app.use("/api/item", itemRouter);
+    this.app.use("/api/category", categoryRouter);
+
+    //logger
+    this.LOGGER = newLogger();
+  }
+
+
+  run(port) {
+    this.app.listen(port, () => {
+      this.LOGGER.info(`server running on running on port ${port}`); 
+    });
+  }
+}
+
+export default Server;
